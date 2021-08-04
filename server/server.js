a727b9ff771323a97d18058c872a354251ff76bf
Yazmin Tinoco <ytip30@gmail.com>
2021-08-04 17:43:09 -0500

    integrate our Apollo server with the Express application as middleware

--- server/server.js
+++ server/server.js
@@ -17,6 +17,9 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
