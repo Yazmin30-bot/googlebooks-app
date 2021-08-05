5246342f78290c346ac9704cef11b8c50646b451
Yazmin Tinoco <ytip30@gmail.com>
2021-08-04 21:19:20 -0500

    Mutations that add a user, login a user, save a book of a user and remove a book of a user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,6 +16,55 @@ const resolvers = {
         throw new AuthenticationError('Not logged in');
       }
     },
+    Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+        
+          if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+        
+          const correctPw = await user.isCorrectPassword(password);
+        
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+        
+          const token = signToken(user);
+          return { token, user };
+        },
+        saveBook: async (parent, { input }, {user}) => {
+          if (user) {
+            const updatedUser = await User.findByIdAndUpdate(
+              { _id: user._id },
+              { $addToSet: { savedBooks: input } },
+              { new: true, runValidators: true }
+            );
+        
+            return updatedUser;
+          }
+          throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (parent, { bookId }, {user}) => {
+            if (user) {
+              const updatedUser = await Thought.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true, runValidators: true }
+              );
+          
+              return updatedUser;
+            }
+          
+            throw new AuthenticationError('You need to be logged in!');
+        }
+      }
     
   };
   
