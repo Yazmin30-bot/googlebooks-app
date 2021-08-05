bf28a652d6254c4fb8d90f65992403ea711d3fe9
Yazmin Tinoco <ytip30@gmail.com>
2021-08-04 21:14:19 -0500

    Query that return a user type

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    //Query that return a user type
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+      
+          return userData;
+        }
+      
+        throw new AuthenticationError('Not logged in');
+      }
+    },
+    
+  };
+  
+  module.exports = resolvers;
