938c73244219f1149ab651f29299b13a232e43e5
Yazmin Tinoco <ytip30@gmail.com>
2021-08-06 19:34:22 -0500

    Deleted the build on gitignore file

---
+++ client/build/precache-manifest.5152830dc49aea3af41e610385f83d9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc5a21ee00e055b48e50c53aa35800b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b276d524e51ff0ee4fa7",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "05ad9ef7ca92b3fbb0db",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "b276d524e51ff0ee4fa7",
+    "url": "/static/js/2.7723180b.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.7723180b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ad9ef7ca92b3fbb0db",
+    "url": "/static/js/main.0294b88c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
