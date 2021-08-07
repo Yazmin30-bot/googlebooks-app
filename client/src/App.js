389f0c00af02853f7dcad0f3d68f98f5011534e5
Yazmin Tinoco <ytip30@gmail.com>
2021-08-06 19:48:43 -0500

    Create set up for develop

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ import Navbar from './components/Navbar';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'https://peaceful-springs-21820.herokuapp.com/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
--- server/server.js
+++ server/server.js
@@ -20,13 +20,14 @@ const server = new ApolloServer({
 });
 
 // integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
+server.applyMiddleware({ app , path: "/graphql"});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
+  
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
