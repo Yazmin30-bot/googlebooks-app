a2f09d3eb78bd2f3973957f1a5b1e3b7be096e26
Yazmin Tinoco <ytip30@gmail.com>
2021-08-06 20:49:55 -0500

    Changing routes

--- server/server.js
+++ server/server.js
@@ -25,15 +25,18 @@ server.applyMiddleware({ app , path: "/graphql"});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+app.use('/img',express.static(path.join(__dirname, 'public/images')));
+app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
+app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
 
-/* if (process.env.NODE_ENV === 'production') {
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
   
   app.use(express.static(path.join(__dirname, '../client/build')));
-} */
+}
 
 /* app.use(routes); */
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
